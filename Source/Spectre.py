33ccc8372d8c1496c0d9485184b2c8301913fb71
27amsterdam <139158793+27amsterdam@users.noreply.github.com>
2023-08-11 10:57:16 +0200

    super spectre zusammmengesetzt

--- Source/Spectre.py
+++ Source/Spectre.py
@@ -1,24 +1,32 @@
 from manim import *
 
 class Spectre(Scene):
+    super_spectre=[
+        [0,0,0,0],
+        [0,13,5,-30],
+        [0,11,9,120],
+        [1,13,9,90],
+        [1,0,2,-90],
+        [1,13,5,-30],
+        [5,11,5,0],
+        [5,1,9,60],
+        [7,1,9,60]
+    ]
+
+    # from_super_tile_index, from_tile_index, from_corner_index, to_tile_index, to_corner_index, gamma
+    super_spectres=[
+        [0,2,8,2,8,120],
+        [0,2,8,3,8,60]
+    ]
+
     def construct(self):
 
         alphas=[0, -60, 90, 60, 0, 60, -90, 60, 90, 60, -90, 60, 90, -60]
-        super_sperctre=[
-            [0,13,5,-30],
-            [0,11,9,120],
-            [1,13,9,90],
-            [1,0,2,-90],
-            [1,13,5,-30],
-            [5,11,5,0],
-            [5,1,9,60],
-            [7,1,9,60]
-        ]
         direction=[1,0,0]
         focus=DOWN
         spectre_points=[]
         spectre_creation_objects=[]
-        spectre_stroke_width=10
+        spectre_stroke_width=6
 
         text=Text("Spectre Monotile", font_size=30).move_to([0,3.5,0])
         self.add(text)
@@ -40,24 +48,49 @@ class Spectre(Scene):
             focus=line.get_end()  
             spectre_points.append(focus)             
 
-        print("spectre_points: " + str(spectre_points))
         spectre_polygon=Polygon(*spectre_points, color=GREY, stroke_width=spectre_stroke_width)
         spectre_polygon.set_fill(GREEN)
         spectre_polygon.set_opacity(1)
         self.play(FadeIn(spectre_polygon),FadeOut(*spectre_creation_objects), run_time=1)
-        self.wait(3)
+        self.wait(1)
 
         # kunstruiere Super-Kachel
-        print("spectre_polygon: " + str(spectre_polygon.points))
+        first_tile_index=1
+        last_tile_index=-1
+        super_tile=self.draw_super_tile(spectre_polygon, first_tile_index, last_tile_index)
+
+        self.play(super_tile.animate.scale(0.5).shift([-1,0,0]), run_time=0.5)
+        super_tiles=[super_tile]
+        for super_spectre in self.super_spectres:
+            from_super_tile_index, from_tile_index, from_corner_index, to_tile_index, to_corner_index, gamma = super_spectre
+            # copy, translate and rotate supertile
+            from_super_tile=super_tiles[from_super_tile_index]
+            from_tile=from_super_tile[from_tile_index]
+            from_tile_vertices=from_tile.get_vertices()
+            from_corner=from_tile_vertices[from_corner_index]
+            to_tile=from_super_tile[to_tile_index]
+            to_tile_vertices=to_tile.get_vertices()
+            to_corner=to_tile_vertices[to_corner_index]
+            translation_vector=to_corner-from_corner
+       
+            to_super_tile=from_super_tile.copy()
+            self.add(to_super_tile)
+            self.play(to_super_tile.animate.shift(translation_vector), run_time=0.5)
+            self.play(Rotating(to_super_tile, about_point=to_corner, radians= gamma *DEGREES, run_time=0.3))
+
+            super_tiles.append(to_super_tile)
+
+        self.wait(3) 
+
+    def draw_super_tile(self, spectre_polygon, first_tile_index, last_tile_index):
+        self.play(spectre_polygon.animate.scale(0.5).shift([-2,-2,0]), run_time=0.5)
         spectre_tiles=[spectre_polygon]
-        for spectre in super_sperctre:
+        for spectre in self.super_spectre[first_tile_index:last_tile_index]:
             from_tile_index, from_corner_index, to_corner_index, beta = spectre
             from_tile=spectre_tiles[from_tile_index]
             from_tile_vertices=from_tile.get_vertices()
             from_corner=from_tile_vertices[from_corner_index]
             to_corner=from_tile_vertices[to_corner_index]
-            print("from_corner: " + str(from_corner))
-            print("to_corner: " + str(to_corner))
             translation_vector=to_corner-from_corner
 
             to_tile=from_tile.copy()
@@ -65,6 +98,5 @@ class Spectre(Scene):
             self.play(to_tile.animate.shift(translation_vector), run_time=0.5)
             self.play(Rotating(to_tile, about_point=to_corner, radians= beta *DEGREES, run_time=0.3))
 
-            spectre_tiles.append(to_tile)
-
-        self.wait(3)           
+            spectre_tiles.append(to_tile) 
+        return VGroup(*spectre_tiles)         
