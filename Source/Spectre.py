e33bce5ff831e747d0ea4574e4d596909f23c3f0
27amsterdam <139158793+27amsterdam@users.noreply.github.com>
2023-08-12 12:01:52 +0200

    dictionary, tuple und algorythmus (...) gelernt

--- Source/Spectre.py
+++ Source/Spectre.py
@@ -1,4 +1,5 @@
 from manim import *
+import numpy as np
 
 class Spectre(Scene):
     super_spectre=[
@@ -18,6 +19,8 @@ class Spectre(Scene):
         [0,2,8,2,8,120],
         [0,2,8,3,8,60]
     ]
+
+    decimals=4
 
     def construct(self):
 
@@ -59,6 +62,10 @@ class Spectre(Scene):
         last_tile_index=-1
         super_tile=self.draw_super_tile(spectre_polygon, first_tile_index, last_tile_index)
 
+        envelope_points = self.get_envelope_points(super_tile)        
+
+
+
         self.play(super_tile.animate.scale(0.5).shift([-1,0,0]), run_time=0.5)
         super_tiles=[super_tile]
         for super_spectre in self.super_spectres:
@@ -99,4 +106,31 @@ class Spectre(Scene):
             self.play(Rotating(to_tile, about_point=to_corner, radians= beta *DEGREES, run_time=0.3))
 
             spectre_tiles.append(to_tile) 
-        return VGroup(*spectre_tiles)         
+        return VGroup(*spectre_tiles)  
+
+    def get_envelope_points(self, polygons : VGroup):
+        envelope_points = self.get_rounded_points(polygons[0])
+        for polygon in polygons[1 : -1]:
+            rounded_polygon_points = self.get_rounded_points(polygon) 
+            frequency_by_point = dict()
+            for envelope_point in envelope_points:
+                frequency_by_point[envelope_point] = 1
+            for polygon_point in rounded_polygon_points:
+                frequency_by_point[polygon_point] = frequency_by_point.setdefault(polygon_point, 0) + 1
+
+            neighbours_by_point = dict()
+            for index, envelope_point in enumerate(envelope_points):
+                next_point = envelope_points[(index + 1) % len(envelope_points)]
+                previous_point = envelope_points[(index - 1) % len(envelope_points)]
+                neighbours_by_point[envelope_point] = neighbours_by_point.setdefault(envelope_point, set()).add(next_point).add(previous_point)
+            for polygon_point in rounded_polygon_points:
+                next_point = polygon_point[(index + 1) % len(rounded_polygon_points)]
+                previous_point = polygon_point[(index - 1) % len(rounded_polygon_points)]
+                neighbours_by_point[polygon_point] = neighbours_by_point.setdefault(polygon_point, set()).add(next_point).add(previous_point)
+           
+
+        
+               
+    def get_rounded_points(self, polygon : Polygon):
+        return list(map(tuple, polygon.get_vertices().round(self.decimals)))
+
